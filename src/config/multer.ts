1038723c90084304a63c243b0dd796f842cc5b4d
iury.magano <iury.magano@beesix.com.br>
2023-12-24 15:44:28 -0300

    update upload image

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,12 +1,22 @@
 import multer from "multer";
+import fs from "fs";
 
 import path from "path";
 
+const destinationPath = "./src/uploadsd";
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "src/uploads/");
+    console.log(file, "multer disk");
+    cb(null, destinationPath);
+
+    if (!fs.existsSync(destinationPath)) {
+      // Se não existe, cria o diretório
+      fs.mkdirSync(destinationPath);
+    }
   },
   filename: (req, file, cb) => {
+    console.log(file, "multer filename");
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
